1f20c1ce8090764b2f402f6ecd14172ea0db7919
German Moyano <c.germanmoyano@gmail.com>
2017-01-04 00:31:48 -0300

    Creating activities for exercise 1, 2 and 3

---
+++ Lesson1/app/src/main/java/com/androidtraining/gm/lesson1/Exercise1.java
@@ -0,0 +1,13 @@
+package com.androidtraining.gm.lesson1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercise1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise1);
+    }
+}
--- Lesson1/app/src/main/java/com/androidtraining/gm/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/androidtraining/gm/lesson1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.androidtraining.gm.lesson1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_exercise1 = (Button) findViewById(R.id.button_Exercise1);
+        Button button_exercise2 = (Button) findViewById(R.id.button_Exercise2);
+        Button button_exercise3 = (Button) findViewById(R.id.button_Exercise3);
+
+
+        button_exercise1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent exc1 = new Intent(MainActivity.this, Exercise1.class);
+                startActivity(exc1);
+            }
+        });
+
+        button_exercise2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent exc2 = new Intent(MainActivity.this, Exercise2.class);
+                startActivity(exc2);
+            }
+        });
+
+        button_exercise3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent exc3 = new Intent(MainActivity.this, Exercise3.class);
+                startActivity(exc3);
+            }
+        });
     }
+
+
 }
